deb627a7ea26f9d6cd03d5f0191d3f819a6993a7
julio candeia <juliocandeia594@gmail.com>
2025-04-24 09:56:37 -0300

    chore: inicialize project with react+Vite+tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-purple-700">
+        Chat FURIA no ar! 🔥
+      </h1>
+    </>
+  );
+}
+
+export default App;
